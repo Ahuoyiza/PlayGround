901acb623d59f92c569909777e7455275fe91cc9
sophia <sophieoyiza@gmail.com>
2021-08-14 02:15:49 +0100

    set up

---
+++ src/Landing-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Flex,Heading } from "@chakra-ui/react";
+
+
+const Home = () => {
+    return (
+        <Flex>
+            <Heading>Home</Heading>
+        </Flex>
+    )
+}
+
+export default Home
